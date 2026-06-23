c46319d542bdf096e077b87941e0f4748ca4bc79
Ikonizx <34430777+Ikonizx@users.noreply.github.com>
2026-06-23 15:17:25 -0500

    Return file key from CreateFile and return public URL in media upload handlers (BUCKET_DOMAIN)

--- internal/http/publicapi/media_group.go
+++ internal/http/publicapi/media_group.go
@@ -2,8 +2,8 @@ package publicapi
 
 import (
 	"net/http"
+	"os"
 
-	"github.com/fivemanage/lite/internal/auth"
 	"github.com/fivemanage/lite/internal/http/httputil"
 	"github.com/fivemanage/lite/internal/http/middleware"
 	"github.com/fivemanage/lite/internal/service/file"
@@ -14,96 +14,69 @@ import (
 
 // DRY they said
 func registerMediaApi(group *echo.Group, fileService *file.Service, tokenService *token.Service, cache *cache.Cache) {
-	h := &mediaHandler{fileService: fileService}
-	group.POST("/image", h.uploadImage, middleware.TokenAuth(tokenService, cache), middleware.ValidateMime("image", middleware.WhitelistedImageMIME))
-	group.POST("/video", h.uploadVideo, middleware.TokenAuth(tokenService, cache), middleware.ValidateMime("video", middleware.WhitelistedVideoMIME))
-	group.POST("/audio", h.uploadAudio, middleware.TokenAuth(tokenService, cache), middleware.ValidateMime("audio", middleware.WhitelistedAudioMIME))
-	group.POST("/file", h.uploadFile, middleware.TokenAuth(tokenService, cache), middleware.ValidateMime("file", nil))
-}
+	group.POST("/image", func(c echo.Context) error {
+		var err error
+		ctx := c.Request().Context()
 
-type mediaHandler struct {
-	fileService *file.Service
-}
+		file, header, err := httputil.File(c.Request(), "image")
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, echo.Map{
+				"error": err.Error(),
+			})
+		}
 
-// uploadImage godoc
-// @Summary      Upload image
-// @Description  Upload an image file
-// @Tags         public
-// @Accept       multipart/form-data
-// @Produce      json
-// @Param        image  formData  file  true  "Image file"
-// @Success      200    {object}  httputil.ResponseData{data=string}
-// @Failure      401    {object}  httputil.ErrorResponseData
-// @Failure      500    {object}  httputil.ErrorResponseData
-// @Router       /image [post]
-func (h *mediaHandler) uploadImage(c echo.Context) error {
-	return h.handleUpload(c, "image")
-}
+		key, err := fileService.CreateFile(ctx, "", file, header)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, err)
+		}
 
-// uploadVideo godoc
-// @Summary      Upload video
-// @Description  Upload a video file
-// @Tags         public
-// @Accept       multipart/form-data
-// @Produce      json
-// @Param        video  formData  file  true  "Video file"
-// @Success      200    {object}  httputil.ResponseData{data=string}
-// @Failure      401    {object}  httputil.ErrorResponseData
-// @Failure      500    {object}  httputil.ErrorResponseData
-// @Router       /video [post]
-func (h *mediaHandler) uploadVideo(c echo.Context) error {
-	return h.handleUpload(c, "video")
-}
+		bucket := os.Getenv("BUCKET_DOMAIN")
+		url := bucket + "/" + key
 
-// uploadAudio godoc
-// @Summary      Upload audio
-// @Description  Upload an audio file
-// @Tags         public
-// @Accept       multipart/form-data
-// @Produce      json
-// @Param        audio  formData  file  true  "Audio file"
-// @Success      200    {object}  httputil.ResponseData{data=string}
-// @Failure      401    {object}  httputil.ErrorResponseData
-// @Failure      500    {object}  httputil.ErrorResponseData
-// @Router       /audio [post]
-func (h *mediaHandler) uploadAudio(c echo.Context) error {
-	return h.handleUpload(c, "audio")
-}
+		return c.JSON(200, echo.Map{"url": url})
+	}, middleware.TokenAuth(tokenService, cache))
 
-// uploadFile godoc
-// @Summary      Upload file
-// @Description  Upload a general file
-// @Tags         public
-// @Accept       multipart/form-data
-// @Produce      json
-// @Param        file  formData  file  true  "File"
-// @Success      200   {object}  httputil.ResponseData{data=string}
-// @Failure      401   {object}  httputil.ErrorResponseData
-// @Failure      500   {object}  httputil.ErrorResponseData
-// @Router       /file [post]
-func (h *mediaHandler) uploadFile(c echo.Context) error {
-	return h.handleUpload(c, "file")
-}
+	group.POST("/video", func(c echo.Context) error {
+		var err error
+		ctx := c.Request().Context()
 
-func (h *mediaHandler) handleUpload(c echo.Context, fileType string) error {
-	var err error
-	ctx := c.Request().Context()
+		file, header, err := httputil.File(c.Request(), "video")
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, echo.Map{
+				"error": err.Error(),
+			})
+		}
 
-	orgId, err := auth.CurrentOrgId(c)
-	if err != nil {
-		// we should probably have a better error here
-		return echo.NewHTTPError(http.StatusUnauthorized, httputil.ErrorResponse("Unauthorized: "+err.Error()))
-	}
+		key, err := fileService.CreateFile(ctx, "", file, header)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, err)
+		}
 
-	file, header, err := httputil.File(c.Request(), fileType)
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, httputil.ErrorResponse(err.Error()))
-	}
+		bucket := os.Getenv("BUCKET_DOMAIN")
+		url := bucket + "/" + key
 
-	err = h.fileService.CreateFile(ctx, orgId, file, header)
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, httputil.ErrorResponse(err.Error()))
-	}
+		return c.JSON(200, echo.Map{"url": url})
+	})
 
-	return c.JSON(200, httputil.Response("ok"))
+	group.POST("/audio", func(c echo.Context) error {
+		var err error
+		ctx := c.Request().Context()
+
+		file, header, err := httputil.File(c.Request(), "audio")
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, echo.Map{
+				"error": err.Error(),
+			})
+		}
+
+		key, err := fileService.CreateFile(ctx, "", file, header)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, err)
+		}
+
+		bucket := os.Getenv("BUCKET_DOMAIN")
+		url := bucket + "/" + key
+
+		return c.JSON(200, echo.Map{"url": url})
+	})
 }
